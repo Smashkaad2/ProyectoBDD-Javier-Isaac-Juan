6c3e1f1dfbfe1e3f85a85af72cc8dd8b99e4df2a
Spdrd <juandiegoreyesrodriguez@gmail.com>
2023-11-23 10:50:48 -0500

    cosa

--- ProyectoBDD/src/main/java/com/example/proyectobdd/Aplicacion.java
+++ ProyectoBDD/src/main/java/com/example/proyectobdd/Aplicacion.java
@@ -14,33 +14,29 @@ public class Aplicacion extends Application {
     private Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
-        try {
-            this.primaryStage = stage;
-            FXMLLoader fxmlLoader = new FXMLLoader(Aplicacion.class.getResource("iniciarSesion.fxml"));
-            AnchorPane pane = fxmlLoader.load();
-            ControllerIniciarSesion controller = fxmlLoader.getController();
-            controller.setAplicacion(this);
-            Scene scene = new Scene(pane);
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
-        }
-        catch (IOException e){
-
-        }
+        this.primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Aplicacion.class.getResource("iniciarSesion.fxml"));
+        AnchorPane pane = fxmlLoader.load();
+        ControllerIniciarSesion controller = fxmlLoader.getController();
+        controller.setAplicacion(this);
+        Scene scene = new Scene(pane);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
     public void abrirPagarFactura(){
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Aplicacion.class.getResource("buscarMascotas.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Aplicacion.class.getResource("agendarCita.fxml"));
             AnchorPane pane = fxmlLoader.load();
-            ControllerPagarFactura controller = fxmlLoader.getController();
+            ControllerAgendarCita controller = fxmlLoader.getController();
             controller.setAplicacion(this);
             Scene scene = new Scene(pane);
             primaryStage.setScene(scene);
             primaryStage.show();
         }
         catch (IOException e){
-            System.out.println("aaaa");
+            System.out.println("MAL");
+
         }
     }
 
--- ProyectoBDD/src/main/java/com/example/proyectobdd/controller/ControllerAgendarCita.java
+++ ProyectoBDD/src/main/java/com/example/proyectobdd/controller/ControllerAgendarCita.java
@@ -1,8 +1,14 @@
 package com.example.proyectobdd.controller;
 
+import com.example.proyectobdd.Aplicacion;
 import javafx.event.ActionEvent;
 
 public class ControllerAgendarCita {
+    private Aplicacion aplicacion;
     public void onClickAgendar(ActionEvent actionEvent) {
+    }
+
+    public void setAplicacion(Aplicacion aplicacion) {
+        this.aplicacion = aplicacion;
     }
 }
